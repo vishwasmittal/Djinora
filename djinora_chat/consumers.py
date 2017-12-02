fae3dfb7d42cc1761459a47b4627b15c17468f36
vishwas78 <vishwas.mittal78@gmail.com>
2017-12-02 23:00:32 +0530

    added username session key

--- djinora_chat/consumers.py
+++ djinora_chat/consumers.py
@@ -1,19 +1,18 @@
-from channels.handler import AsgiHandler
 from channels import Channel, Group
 from channels.sessions import channel_session
 
+from urllib.parse import parse_qs
 import json
 
 
 @channel_session
 def ws_connect(message):
-    if 'username' in message.content['text']:
-        message.reply_channel.send({'accept': True})
-        username = message.content['text']['username']
+    message.reply_channel.send({'accept': True})
+    params = parse_qs(message.content['query_string'].decode())
+    if 'username' in params:
+        username = params['username'][0]
         message.channel_session['username'] = username
         Group('public').add(message.reply_channel)
-        username = message.channel_session['username']
-        Group('public').discard(message.reply_channel)
         Group('public').send({
             "text": json.dumps({
                 "text": "has joined the channel",
@@ -27,6 +26,8 @@ def ws_connect(message):
 
 @channel_session
 def ws_receive(message):
+    if 'username' not in message.channel_session:
+        message.reply_channel.disconnect(message)
     sender = message.channel_session['username']
     Group('public').send({
         "text": json.dumps({
